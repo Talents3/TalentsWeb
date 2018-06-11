5c796cc0cfe95444cb98a1c9b9df3d9e9a34bf12
whynjust <hw1456@nyu.edu>
2018-06-11 17:34:01 -0400

    * modified navbar login function

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -11,34 +11,17 @@ import {Subscription} from 'rxjs';
 export class NavbarComponent implements OnInit {
 
   title = "Talents3";
-  _isLoggedIn: false;
+  _isLoggedIn: boolean;
   subscription: Subscription;
 
   constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
-    this.loggedIn();
   }
-
 
   logout() {
-    console.log(this.authService.getToken());
-    console.log(this.authService.getEmail());
     this.authService.logout();
-    document.getElementById("logOutButton").style.display="none";
-    document.getElementById("logInButton").style.display="inline";
-    document.getElementById("SignUpButton").style.display="inline";
     this.router.navigate(['users']);
-    console.log("logout");
-  }
-
-  loggedIn() {
-    this.subscription = this.authService.isUserAuthenticated().subscribe(_isLoggedIn => this._isLoggedIn = _isLoggedIn);
-    if (this.authService.isUserAuthenticated()) {
-      document.getElementById("logInButton").style.display="none";
-      document.getElementById("SignUpButton").style.display="none";
-      document.getElementById("logOutButton").style.display="inline";
-    };
   }
 
 }
