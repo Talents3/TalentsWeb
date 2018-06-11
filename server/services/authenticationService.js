1dc9d3d61443722d321be264be0a6abad66461b2
youxing <yg1497@nyu.edu>
2018-06-11 18:36:52 -0400

    add logic for localstorage data

--- server/services/authenticationService.js
+++ server/services/authenticationService.js
@@ -48,7 +48,7 @@ const login = function(req, res) {
           // if user is found and password is right create a token
           var token = jwt.sign(payload, config.secret);
           // return the information including token as JSON
-          res.json({success: true, token: token, email: req.body.email, username: req.body.username});
+          res.json({success: true, token: token, email: req.body.email, username: user.username});
         } else {
           res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
         }
