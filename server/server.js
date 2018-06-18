7cc545383feed69a3c1491981ad661dcfa73724e
youxing <yg1497@nyu.edu>
2018-06-18 18:31:59 -0400

    add ttl for Temp user, and listenning on env not localhost

--- server/server.js
+++ server/server.js
@@ -132,4 +132,4 @@ app.use(function(req, res, next) {
 // });
 
 //launch application,listen on port3000
-app.listen(3000, () => console.log('Example app listening on port3000!'));
+app.listen(process.env.PORT, process.env.IP, () => console.log('Example app listening !'));
